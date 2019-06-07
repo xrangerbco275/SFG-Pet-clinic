34f96667a0d866e345e418d759a402fee48f503c
joe cruz <xrangerbco275@att.net>
2019-06-07 12:56:41 -0500

    Added PetTypeMapService. Close #29

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,7 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Speciality
-        extends BaseEntity
+public class Speciality extends BaseEntity
 {
     private String description;
 
